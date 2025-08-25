1c2cad1f99c8a35788afaa930fc7c97702ecdaf2
Dikshafudugo <dikshadekatefwork@gmail.com>
2025-08-25 10:50:44 +0530

    new changes

--- routes/web.php
+++ routes/web.php
@@ -28,22 +28,14 @@ Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('voyager.dashboard');
-    // Route::post('/login', 'App\Http\Controllers\LoginController@login')->name('login');
     Route::post('complaints/import', [ComplaintController::class, 'import'])
         ->name('voyager.complaints.import');
 
 });
 Route::get('/stations', 'App\Http\Controllers\Admin\StationsController@index')->name('stations.index');
-
     Route::get('/register', [UserController::class, 'create'])->name('user.register');
     Route::post('/register', [UserController::class, 'store'])->name('user.store');
     Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
     Route::post('/user/{id}/update', [UserController::class, 'update'])->name('user.update');
-// Route::group(['prefix' => 'admin', 'middleware' => ['web', 'admin.user']], function () {
-//     Route::get('/stations', 'App\Http\Controllers\StationsController@index')->name('voyager.index');
-//     // Route::get('/stations', [StationsController::class, 'statistics'])
-//     //     ->name('voyager.stations.statistics');
-// });
-
 
 
