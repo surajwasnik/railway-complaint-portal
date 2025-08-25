95d136383726fe63dfd2f4e6c8daf6a9a0059288
Dikshafudugo <dikshadekatefwork@gmail.com>
2025-08-25 10:47:51 +0530

    new changes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\ComplaintController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
+use TCG\Voyager\Facades\Voyager;
 use TCG\Voyager\Http\Controllers\StationsController;
 
 /*
@@ -17,33 +19,31 @@ use TCG\Voyager\Http\Controllers\StationsController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
-    Route::get('/', [DashboardController::class, 'index'])->name('voyager.dashboard');
-    Route::post('/login', 'App\Http\Controllers\LoginController@login')->name('login');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('voyager.dashboard');
+    // Route::post('/login', 'App\Http\Controllers\LoginController@login')->name('login');
+    Route::post('complaints/import', [ComplaintController::class, 'import'])
+        ->name('voyager.complaints.import');
+
 });
+Route::get('/stations', 'App\Http\Controllers\Admin\StationsController@index')->name('stations.index');
+
+    Route::get('/register', [UserController::class, 'create'])->name('user.register');
+    Route::post('/register', [UserController::class, 'store'])->name('user.store');
+    Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+    Route::post('/user/{id}/update', [UserController::class, 'update'])->name('user.update');
 // Route::group(['prefix' => 'admin', 'middleware' => ['web', 'admin.user']], function () {
 //     Route::get('/stations', 'App\Http\Controllers\StationsController@index')->name('voyager.index');
 //     // Route::get('/stations', [StationsController::class, 'statistics'])
 //     //     ->name('voyager.stations.statistics');
 // });
 
-Route::get('/stations', 'App\Http\Controllers\Admin\StationsController@index')->name('stations.index');
-
-Route::get('/register', [UserController::class, 'create'])->name('user.register');
-Route::post('/register', [UserController::class, 'store'])->name('user.store');
-Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
-Route::post('/user/{id}/update', [UserController::class, 'update'])->name('user.update');
 
 
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-
-    Route::post('complaints/import', [ComplaintController::class, 'import'])
-        ->name('voyager.complaints.import');
-});
